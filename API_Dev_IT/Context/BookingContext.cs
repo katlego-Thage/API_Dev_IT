c0e1a607899b1e6518c360893301661f1dac4813
Katlego Thage <katlego@esoftwaresolutions.co.za>
2026-04-01 23:38:59 +0200

    User Controller. Get single user and all user

--- API_Dev_IT/Context/BookingContext.cs
+++ API_Dev_IT/Context/BookingContext.cs
@@ -10,7 +10,6 @@ namespace API_Dev_IT.Context
         }
         public DbSet<Model.User> User { get; set; }
         public DbSet<Model.Booking> booking { get; set; }
-        public DbSet<Model.LogIn> logIn { get; set; }
         public DbSet<Model.Payment> payment { get; set; }
         public DbSet<Model.Role> role { get; set; }
         public DbSet<Model.Room> room { get; set; }
---
+++ API_Dev_IT/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using API_Dev_IT.Context;
+using API_Dev_IT.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System.Collections.Immutable;
+
+namespace API_Dev_IT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly BookingContext _context;
+        private readonly ILogger<UserController> _logger;
+        public UserController(BookingContext context, ILogger<UserController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetUser")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var user = await _context
+                                 .User
+                                 .AsNoTracking()
+                                 .ToListAsync();
+                return Ok(user);
+            }
+            catch(Exception x)
+            {
+                _logger.LogError($"{x.Message}");
+                return BadRequest(x.Message);
+            }
+        }
+
+        [HttpGet("GetUser/{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var user = await _context
+                                 .User
+                                 .FirstAsync
+                                 (x => x.UserID == id);
+                return Ok(user);
+            }
+            catch(Exception x)
+            {
+                _logger.LogError($"{x.Message}");
+                return BadRequest(x.Message);
+            }
+        }
+    }
+}
