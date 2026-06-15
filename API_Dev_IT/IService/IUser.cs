385d1ef1a6fe66ce21036962e1c4e8a22c2f8a25
Katlego Thage <katlego@esoftwaresolutions.co.za>
2026-06-15 18:33:50 +0200

    updated User Interface IUser

--- API_Dev_IT/IService/IUser.cs
+++ API_Dev_IT/IService/IUser.cs
@@ -4,11 +4,8 @@ namespace API_Dev_IT.IService
 {
     public interface IUser
     {
-        //Task<User> LogIn(LogIn logIn);
-
         Task<T> LogIn<T> (LogIn logIn);
-            //public List<T> GetItems<T>()
-        Task<User> Create(User user);
+        Task<T> Create<T>(User user);
         Task<User> Update(User user, int id);
         Task<User> Delete(int id);
     }
